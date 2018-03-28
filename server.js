587d458b754982806695f4458b9f4fa2071df4a0
desshub <desshub95@gmail.com>
2018-03-28 16:16:44 +0300

    refactor to ES6

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express=require('express');
+const bodyParser = require('body-parser');
 const  hostname= 'localhost'
 const port=8080;
 
 
 const app =express();
 
-
-app.get('/', function(req,res){
+app.use(bodyParser());
+app.get('/', (req,res)=>{
     res.send('helo world')
 }   )
+app.post('/add' ,(req, res)=>{
+    let name=req.body.name;
+    let email=req.body.email;
+    let age =req.body.age;
+
+    var resp = {
+        name: name,
+        email: email,
+        age: age
+    };
+     console.log(resp);
+    res.send(resp);
+})
 
 
 app.listen( port, function () {
