5af4ae3894a75cd054fabb999458c04f607c3b7f
desshub <desshub95@gmail.com>
2018-04-05 18:36:04 +0300

    implemented update

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,26 @@ router.get('/user:id', (req, res) => {  // Endpoint to retrieve single user item
      res.json(user);
 });
 
+router.post('/adduser', (req, res) => {
+    
+    let name = req.body.name;
+    let description = req.body.description;
+    let date = req.body.date;
+
+    let user = addUser(name, description, date);
+    res.json(user);
+})
+
+router.put('/updateuser:id', (req, res) => {
+    let id = req.params.id
+    let name = req.body.name;
+    let description = req.body.description;
+    let date = req.body.date;
+
+    let user = updateUser(id, name, description, date);
+    res.json(user);
+})
+
 let user = {};
 let users = [];
 console.log("top " + users );
@@ -44,7 +64,27 @@ let aly = {
 users.push(jemimah);  // Adding items to usrs object
 users.push(dess);
 users.push(aly);
+let addUser = (name, description, date) => {
+    user.id = users.length + 1;
+    user.name = name;
+    user.description = description;
+    user.date = date;
+    users.push(user);
+    return user;
+
+}
 console.log("bottom " + users);
+
+let updateUser = (id, name, description, date) => {
+    for(let x=0; x<users.length; x++){
+        if(users[x].id == id){
+            users[x].name = name;
+            users[x].description = description;
+            users[x].date = date;
+            return users[x];
+        }
+    }
+}
 
 let getUsers = () => {  // Function/method to retrieve all the users
     return users;
