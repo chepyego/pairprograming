fc90428f1c66c9297a2a5bfd83650cc45a23a61d
desshub <desshub95@gmail.com>
2018-04-05 17:43:30 +0300

    completed task 3 and 4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=> res.json("It's getting router"));
+router.get('/', (req, res)=> {
+    
+    res.json(getUsers());
+});
+
+let user = {};
+let users = [];
+let jemimah = {
+    id: 1,
+    name : "Jemimah",
+    description : "Cute",
+    date : "5/04/2018"
+};
+let dess = {
+    id: 1,
+    name : "Dess",
+    description : "Nerd",
+    date : "5/04/2018"
+};
+let aly = {
+    id: 1,
+    name : "Aly",
+    description : "Geeky",
+    date : "5/04/2018"
+};
+
+
+users.push(jemimah);
+users.push(dess);
+users.push(aly);
+
+let getUsers = () => {
+    return users;
+}
+
 
 module.exports = router;
