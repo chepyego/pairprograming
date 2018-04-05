099cadd24db9a814bf241648266451a0e9dc952d
desshub <desshub95@gmail.com>
2018-04-05 18:09:42 +0300

    created getUser method

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res)=> {
+router.get('/', (req, res)=> { // index endpoint
     
-    res.json(getUsers());
+    res.json("WELCOME TO THIS MILESTONE");
+});
+
+router.get('/users', (req, res)=> {   // Endpoint to retrieves all the users
+    let users = getUsers();      // Assign getUsers function to variable users
+    res.json(users);  // Send results in form of json
+})
+
+router.get('/user:id', (req, res) => {  // Endpoint to retrieve single user item
+     let id = req.params.id; // id from the endpoint 
+     let user = getUser(id); 
+     res.json(user);
 });
 
 let user = {};
 let users = [];
+console.log("top " + users );
+
 let jemimah = {
     id: 1,
     name : "Jemimah",
@@ -15,25 +28,34 @@ let jemimah = {
     date : "5/04/2018"
 };
 let dess = {
-    id: 1,
+    id: 2,
     name : "Dess",
     description : "Nerd",
     date : "5/04/2018"
 };
 let aly = {
-    id: 1,
+    id: 3,
     name : "Aly",
     description : "Geeky",
     date : "5/04/2018"
 };
 
 
-users.push(jemimah);
+users.push(jemimah);  // Adding items to usrs object
 users.push(dess);
 users.push(aly);
+console.log("bottom " + users);
 
-let getUsers = () => {
+let getUsers = () => {  // Function/method to retrieve all the users
     return users;
+}
+
+let getUser = (id) => {   // Function to retrieve singler user item
+   for(let x=0; x<users.length; x++){
+       if(users[x].id == id){
+           return users[x];
+       }
+   }
 }
 
 
